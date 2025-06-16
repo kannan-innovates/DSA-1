7c7c60be74a72a6d4e785f35c62b33ea894cc263
Kannan <kannans02001@gmail.com>
2025-06-17 02:02:46 +0530

    learned linked list basics and how to append

--- Array/palindrome-number.js
+++ Array/palindrome-number.js
@@ -1,26 +1,26 @@
 
-let isPalindrome = true;
-let arr = [1,2,3,4,3,2,1,0];
-for(let i = 0; i < arr.length; i++){
-     for (let j = arr.length-1-i; j >= arr.length-1-i; j --){
-          if(arr[i]!== arr[j]){
-               isPalindrome = false;
-          }
-     }
-}
-console.log(isPalindrome)
+// let isPalindrome = true;
+// let arr = [1,2,3,4,3,2,1,0];
+// for(let i = 0; i < arr.length; i++){
+//      for (let j = arr.length-1-i; j >= arr.length-1-i; j --){
+//           if(arr[i]!== arr[j]){
+//                isPalindrome = false;
+//           }
+//      }
+// }
+// console.log(isPalindrome)
 
 
-let arr = [1,2,3,4,3,2,1];
-let isPalindrome = true;
-for(let i = 0; i < Math.floor(arr.length/2); i++){
-     if(arr[i]!== arr[arr.length-1-i]){
-          isPalindrome = false;
-          break;
-     }
-}
+// let arr = [1,2,3,4,3,2,1];
+// let isPalindrome = true;
+// for(let i = 0; i < Math.floor(arr.length/2); i++){
+//      if(arr[i]!== arr[arr.length-1-i]){
+//           isPalindrome = false;
+//           break;
+//      }
+// }
 
-console.log(isPalindrome)
+// console.log(isPalindrome)
 
 
 
---
+++ Linked-List/Single-Linked-List/sll-append.js
@@ -0,0 +1,37 @@
+class Node{
+     constructor(data){
+          this.data = data;
+          this.next =null;
+     }
+}
+class SinglyLinkedList{
+     constructor(){
+          this.head = null;
+     }
+     append(data){
+          let newNode = new Node(data);
+          if(this.head === null){
+               this.head = newNode;
+          }else{
+               let current = this.head;
+               while(current.next !== null){
+                    current = current.next;
+               }
+               current.next = newNode;
+          }
+          
+     }
+     printList(){
+          let current = this.head;
+          while(current !== null){
+               console.log(current.data);
+               current = current.next;
+          }
+     }
+}
+
+let list = new SinglyLinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.printList();
