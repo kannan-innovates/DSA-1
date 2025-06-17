813d661a6c6abc379c55c6fe22a3e4feff1c7803
Kannan <kannans02001@gmail.com>
2025-06-17 14:42:09 +0530

    learned how to insert an element at a certain positin handling all edge cases in a Single Linked List

---
+++ Linked-List/Single-Linked-List/sll-insert-at-position.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+class SinglyLinkedList {
+  constructor(head) {
+    this.head = null;
+  }
+  append(data) {
+    let newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+  insertAtPosition(pos, data) {
+     let newNode = new Node(data)
+    if (pos === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    let index = 0;
+
+    while (index < pos - 1 && current !== null) {
+      current = current.next;
+      index++;
+    }
+    if (current === null) {
+      console.log(`Position is out bonus`);
+      return;
+    }
+    newNode.next = current.next;
+    current.next = newNode;
+  }
+
+  printList(){
+     let current = this.head;
+     while(current !== null){
+          console.log(current.data);
+          current = current.next;
+     }
+  }
+}
+
+let list = new SinglyLinkedList();
+list.append(10);
+list.append(20);
+list.insertAtPosition(1,15);
+list.printList()
