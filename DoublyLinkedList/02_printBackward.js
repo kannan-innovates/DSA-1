8219571a3a79261090f3b7379eb5b77903091074
Kannan <kannans02001@gmail.com>
2025-07-12 12:18:31 +0530

    print backward program of dll

---
+++ DoublyLinkedList/02_printBackward.js
@@ -0,0 +1,45 @@
+class Node{
+     constructor(data){
+          this.data = data;
+          this.next = null;
+          this.prev = null;
+     }
+}
+class DoublyLinkedList{
+     constructor(){
+          this.head = null;
+     }
+     append(data){
+          let newNode = new Node (data);
+
+          if (this.head === null){
+               this.head = newNode;
+               return;
+          }
+
+          let current = this.head;
+
+          while (current.next !== null){
+               current = current.next;
+          }
+          current.next = newNode;
+          newNode.prev = current;
+     }
+
+     printListBackward(){
+          let current = this.head;
+
+          while (current.next !== null){
+               current = current.next;
+          }
+
+          while (current !== null){
+               console.log(current.data);
+               current = current.prev;
+          }
+     }
+}
+
+let dll = new DoublyLinkedList();
+[10,20,30,40].forEach((val) => dll.append(val));
+dll.printListBackward()
